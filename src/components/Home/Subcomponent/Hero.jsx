1c713fde99538036d34ea5afd8186a3c285365eb
AnishMishra1 <Anishmishra@gmail.com>
2024-02-27 20:13:31 +0530

    ading section

--- src/components/Home/Subcomponent/Hero.jsx
+++ src/components/Home/Subcomponent/Hero.jsx
@@ -1,29 +1,29 @@
 import React from "react";
-import "./Hero.css"
+import "./Hero.css";
 import NavBar from "../../../globalSubComponents/NavBar";
-import sofa from "../assets/sofa.png"
+import sofa from "../assets/lau.png";
 
 const Hero = () => {
   return (
-    <div className="md:h-[35rem] h-auto w-auto bg-[#3B5D50] flex md:flex-row flex-col items-center justify-between">
+    <div className="md:h-[35rem] h-auto w-auto bg-[#3B5D50] flex md:flex-row flex-col items-center justify-evenly">
       <div className="flex flex-col items-start justify-between md:px-[5rem] px-[2rem]">
-        <div className="py-[1rem]">
+        <div className="py-[1rem] px-[2rem]">
           <h1 className="text-white font-sans font-bold md:text-5xl text-3xl text-start">
-            Modern Interior <br /> Design Studio
+            Dry Cleaning & Laundary Delivery <br />
+            Service
           </h1>
         </div>
-        <div className="py-[0.5rem] ">
+        <div className="py-[0.5rem] px-[2rem] ">
           <p className="text-gray-400 font-sans text-start">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br /> Ea, ab.sit  amet consectetur adipisicing
-            elit. Ea, ab.
+            We always ensure the quality of Laundary Service. Our fleet team efficient enough to ensure your schedule collection and delivery
           </p>
         </div>
-        <div className="flex md:flex-row flex-col items-start justify-between gap-2 py-[1rem] ">
+        <div className="flex flex-row  items-start justify-between gap-2 py-[1rem] px-[2rem] ">
           <div className="px-[1.8rem] py-[0.8rem]  bg-[#F9BF29] md:rounded-full rounded-3xl text-white border-2 font-sans">
-            <button>Shop now</button>
+            <button>Now Book</button>
           </div>
           <div className="px-[1rem] py-[0.8rem] bg-transparent border-2 md:rounded-full rounded-3xl text-white font-sans">
-            <button>Shop explore</button>
+            <button>Explore</button>
           </div>
         </div>
       </div>
--- src/components/Home/Subcomponent/WhyChoseus.jsx
+++ src/components/Home/Subcomponent/WhyChoseus.jsx
@@ -5,34 +5,34 @@ import { MdAssignmentReturn } from "react-icons/md";
 import { GrSupport } from "react-icons/gr";
 
 const content = [
-    {
-        icon:LiaShippingFastSolid,
-        title:"Fast & Free Shipping",
-        des:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio provident, similique numquam ipsa neque rem?",
-    },
-    {
-        icon: LuShoppingBag,
-        title:"Easy to Shop",
-        des:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio provident, similique numquam ipsa neque rem?",
-    },
-    {
-        icon: GrSupport,
-        title:"24/7 Support",
-        des:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio provident, similique numquam ipsa neque rem?",
-    },
-    {
-        icon:MdAssignmentReturn,
-        title:"Hassle Free Returns",
-        des:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio provident, similique numquam ipsa neque rem?",
-    },
-]
+  {
+    icon: LiaShippingFastSolid,
+    title: "Drop off",
+    des: "We collect the cloths from your Address at free of cost ",
+  },
+  {
+    icon: LuShoppingBag,
+    title: "Place Order",
+    des: "You can place the order through SMS and Phone number",
+  },
+  {
+    icon: GrSupport,
+    title: "Pick Up",
+    des: "We picked the order as per your convience or from your given address",
+  },
+  {
+    icon: MdAssignmentReturn,
+    title: "Deliver",
+    des: "We deliver the your pack cloth in your Address with free of cost",
+  },
+];
 
 const WhyChoseus = () => {
   return (
     <div className="flex flex-row items-start justify-between h-auto bg-[#EFF2F1] py-[3rem]  ">
       <div className="flex flex-col items-start justify-between md:px-[5rem] px-[2rem] text-[#2F2F2F]">
         <div className="py-[1rem]">
-          <h1 className="text-4xl font-sans font-semibold">Why Choose Us</h1>
+          <h1 className="text-4xl font-sans font-semibold">Easy As One, Two ,Three</h1>
         </div>
         <div>
           <p className="text-gray-600 font-sans ">
@@ -57,7 +57,11 @@ const WhyChoseus = () => {
 
       {/* {image} */}
       <div className="lg:inline-block hidden px-[5rem] py-[2rem] ">
-        <img className="rounded-xl md:w-[70rem]" src="https://themewagon.github.io/furni/images/why-choose-us-img.jpg" alt="" />
+        <img
+          className="rounded-xl md:w-[70rem]"
+          src="https://img.freepik.com/free-photo/medium-shot-smiley-woman-with-apron_23-2148246459.jpg?w=740&t=st=1709044177~exp=1709044777~hmac=fa3f724fce9c385adbc8d65a64ab223b7ee6f8f3f11cebab591e10bfef6deb22"
+          alt=""
+        />
       </div>
     </div>
   );
