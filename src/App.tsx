97018cc7303cc51c57bd00dfa9ef8c62bf6fefe0
ruck-45 <thoughtsfirstborn01@gmail.com>
2024-02-28 13:10:20 +0530

    adding robinhood

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,27 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
 import Home from "./components/Home/Home";
+import { RootState } from "./store/store";
+import { useSelector } from "react-redux";
+
+import NavBar from "./globalSubComponents/NavBar";
+import Robinhood from "./components/Robinhood/Robinhood";
+import Footer from "./globalSubComponents/Footer";
 
 // Local Files
 
-
-
 function App() {
-  
+   const curTab = useSelector((state: RootState) => state.curTab.value);
   return (
     <>
-      <div >
-        
-        
-        <Routes>
-          
-          <Route path="/" element={<Home/>}  />
-         
-        </Routes>
-      </div>
-      
+      {curTab === "Auth" || curTab === "Robinhood" ? null : <NavBar />}
+      <Routes>
+        <Route path="/" element={<Navigate to="/Home" />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Robinhood" element={<Robinhood />} />
+        <Route path="*" element={<Navigate to="/Home" />} />
+      </Routes>
+      {curTab === "Auth" || curTab === "Robinhood" ? null : <Footer />}
     </>
   );
 }
